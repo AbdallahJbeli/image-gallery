599cb7f26fcd2d3b563426ef1bbb6f2a4a6b990c
AbdallahJbeli <abdallahjbeli55@gmail.com>
2024-08-26 17:36:42 +0100

    second commit

--- src/App.js
+++ src/App.js
@@ -1,53 +1,28 @@
 import React, { useState, useEffect } from 'react';
+import ImageCard from './components/ImageCard';
 
 function App() {
   const [images, setImages] = useState([]);
-  const [isLoading, setIsLoding] = useState(true);
+  const [isLoading, setIsLoading] = useState(true);
   const [term, setTerm] = useState('');
 
   useEffect(() => {
     fetch(`https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${term}&image_type=photo&pretty=true`)
       .then(res => res.json())
-      .then(data => console.log(data))
+      .then(data => {
+        setImages(data.hits);
+        setIsLoading(false);
+
+      })
       .catch(err => console.log(err));
   }, []);
 
   return (
-    <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img
-        src="https://images.unsplash.com/photo-1716677951302-9eabc1512445?q=80&w=988&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
-        alt=""
-        className="w-full"
-      />
-      <div className="px-6 py-4">
-        <div className="font-bold text-purple-500 text-xl mb-2">
-          Photo by John Doe
-        </div>
-        <ul>
-          <li>
-            <strong>Views: </strong>
-            4000
-          </li>
-          <li>
-            <strong>Downloads: </strong>
-            300
-          </li>
-          <li>
-            <strong>Likes: </strong>
-            400
-          </li>
-        </ul>
-      </div>
-      <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
+    <div className="container mx-auto">
+      <div className="grid grid-cols-3 gap-4">
+        {images.map(image => (
+          <ImageCard key={image.id} image={image} />
+        ))}
       </div>
     </div>
   );
