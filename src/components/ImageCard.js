20ab6374061d08e97d3a3b49ebbf345493c99b76
AbdallahJbeli <abdallahjbeli55@gmail.com>
2024-08-27 15:54:26 +0100

    final commit

--- src/components/ImageCard.js
+++ src/components/ImageCard.js
@@ -1,13 +1,11 @@
 import React from "react";
 
 function ImageCard({ image }) {
+  const tags = image.tags.split(",");
+
   return (
     <div className="max-w-sm rounded overflow-hidden shadow-lg">
-      <img
-        src={image.webformatURL}
-        alt=""
-        className="w-full"
-      />
+      <img src={image.webformatURL} alt="" className="w-full" />
       <div className="px-6 py-4">
         <div className="font-bold text-purple-500 text-xl mb-2">
           Photo by {image.user}
@@ -28,15 +26,11 @@ function ImageCard({ image }) {
         </ul>
       </div>
       <div className="px-6 py-4">
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag1
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag2
-        </span>
-        <span className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
-          #tag3
-        </span>
+        {tags.map((tag, index) => (
+          <span key={index} className="inline-block bg-gray-200 rounded-full px-3 py-1 text-sm font-semibold text-gray-700 mr-2">
+            #{tag}
+          </span>
+        ))}
       </div>
     </div>
   );
